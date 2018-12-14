a0bf4734f38cae32a8647ab1603862a694c165b3
Ibrahim-Elsanhouri <ibrahimelsanhouri@gmail.com>
2018-12-14 13:04:14 +0300

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        //
+        'name' => $faker->word , 
+        'details' => $faker->paragraph, 
+        'price' => $faker->numberBetween(100,1000) , 
+        'stock' => $faker->randomDigit, 
+        'discount' => $faker->numberBetween(2, 30)
+    ];
+});
